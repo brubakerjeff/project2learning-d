ef31f87c217fe61779861117500c639806d564c3
Jeff Brubaker <brubaker.jeff@gmail.com>
2025-05-10 09:15:58 -0400

    removed sleep file per feedback

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -22,23 +22,44 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
-
+    bool ball_found = false;
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
-    /*  for (size_t i = 0; i < img->height * img->step; i += 3) {
-            int red = img->data[i];
-            int green = img->data[i + 1];
-            int blue = img->data[i + 2];
+    int ver=0;
+    int hor=0;
+    for (size_t i = 0; i < img.height * img.step; i += 3) {
+            int red = img.data[i];
+            int green = img.data[i + 1];
+            int blue = img.data[i + 2];
+            hor = (i / 3) % img.width;
+            ver = (i / 3) / img.width;
             // Next check if you found the white color ball
             if (red == 255 && green == 255 && blue == 255) {
                 // Found a white pixel, which means finding the ball
                 // Next step would be decide how to move based on the
                 // calculated pixel position or coordinates in the image
+                ball_found=true;
+                break;
             }
         
-    }*/
+    }
+    if (!ball_found){
+        drive_robot(0,0);
+    }  
+    else if (hor > img.width*2/3){
+        // Ball is in the left side, turn left
+        drive_robot(0, -0.05);
+        ROS_INFO("Ball found on the right side %i.", hor);        
+     } else if (hor < img.width*1/3){
+        // Ball is in the right side, turn right
+        drive_robot(0, 0.05);
+        ROS_INFO("Ball found on the left side %i.",hor);        
+    } else{
+        // Ball is in the middle, go forward
+        drive_robot(10.0, 0.0);
+      }
 }
 
 int main(int argc, char** argv)
